e48778950deb563ced994843147845069ae9c82e
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2021-10-06 13:57:39 -0400

    update

--- Insertion Sort/insertionSort.java
+++ Insertion Sort/insertionSort.java
@@ -3,14 +3,17 @@ public class insertionSort {
     public static void main(String[] args) {
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
 
-        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex--) {
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
 
             int newElement = intArray[firstUnsortedIndex];
 
-            for (int i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i++) {
+            int i;
 
+            for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
+                intArray[i] = intArray[i - 1];
             }
 
+            intArray[i] = newElement;
         }
 
         for (int i = 0; i < intArray.length; i++) {
