d0286aa6fcb8c72a161688143efdf02b31d56f7a
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2021-10-07 02:06:28 -0400

    update

--- com/datastructures/factorial/factorial.java
+++ com/datastructures/factorial/factorial.java
@@ -3,8 +3,16 @@ package com.datastructures.factorial;
 public class factorial {
 
     public static void main(String[] args) {
-        System.out.println(iterativeFactorial(10));
-        System.out.println(recursiveFactorial(10));
+
+        double startTime1 = System.nanoTime();
+        System.out.println("iterativeFactorial - "+iterativeFactorial(10));
+        double stopTime1 = System.nanoTime();
+        System.out.println((stopTime1 - startTime1)/1000000);
+
+        double startTime2 = System.nanoTime();
+        System.out.println("recursiveFactorial - "+recursiveFactorial(10));
+        double stopTime2 = System.nanoTime();
+        System.out.println((stopTime2 - startTime2)/1000000);
     }
 
     public static int recursiveFactorial(int num) {
