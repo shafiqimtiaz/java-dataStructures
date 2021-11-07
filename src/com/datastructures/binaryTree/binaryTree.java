dfd4e528820bcd133bdff38b56c857ef6f237399
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2021-11-07 14:17:37 -0500

    add binary tree

---
+++ src/com/datastructures/binaryTree/binaryTree.java
@@ -0,0 +1,53 @@
+package datastructures.binaryTree;
+
+public class binaryTree {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class Tree {
+
+        private TreeNode root;
+
+    }
+
+    public static class TreeNode {
+
+        private int data;
+        private TreeNode leftChild;
+        private TreeNode rightChild;
+
+        public void insert(int value) {
+
+        }
+
+        public TreeNode(int data) {
+            this.data = data;
+        }
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public TreeNode getLeftChild() {
+            return leftChild;
+        }
+
+        public void setLeftChild(TreeNode leftChild) {
+            this.leftChild = leftChild;
+        }
+
+        public TreeNode getRightChild() {
+            return rightChild;
+        }
+
+        public void setRightChild(TreeNode rightChild) {
+            this.rightChild = rightChild;
+        }
+    }
+}
